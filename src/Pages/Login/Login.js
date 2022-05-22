90579d99f7636eae84d50005dfb86336fcfb0da9
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-05-22 10:22:26 +0600

    login page some validation

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,35 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+    const [passError, setPassError] = useState('')
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+    };
     return (
         <div>
             <div class="w-4/4 md:w-3/4 lg:w-1/4 mx-auto my-24">
                 <div class=" flex-col lg:flex-row-reverse">
                     <div class="card flex-shrink-0 shadow-2xl bg-base-100">
-                        <div class="card-body">
+                        <form onSubmit={handleSubmit(onSubmit)} class="card-body">
                             <div class="form-control">
                                 <label class="label">
                                     <span class="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email" class="input input-bordered" />
+                                <input className="input input-bordered w-full max-w-xs"
+                                    placeholder='Email'
+                                    type='email'
+                                    {...register("email", {
+                                        required: {
+                                            value: true,
+                                            message: 'Email is required'
+                                        },
+                                        pattern: {
+                                            value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                            message: 'Provide a valid email'
+                                        }
+                                    })} />
+                                <label className="label">
+                                    {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                                    {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                                </label>
                             </div>
                             <div class="form-control">
                                 <label class="label">
                                     <span class="label-text">Password</span>
                                 </label>
-                                <input type="text" placeholder="password" class="input input-bordered" />
-                                <label class="label">
+                                <input className="input input-bordered w-full max-w-xs"
+                                    placeholder='Password'
+                                    type='password'
+                                    {...register("password", {
+                                        required: {
+                                            value: true,
+                                            message: 'Password is Required'
+                                        },
+                                        minLength: {
+                                            value: 6,
+                                            message: 'Must be 6 characters'
+                                        }
+                                    })} />
+                                {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                                {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+
+
+
+                                <label label class="label" >
                                     <p class="label-text-alt link link-hover">Forgot password?</p>
                                 </label>
                             </div>
                             <div class="form-control mt-6">
                                 <button class="btn btn-primary">Login</button>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     );
 };
 
